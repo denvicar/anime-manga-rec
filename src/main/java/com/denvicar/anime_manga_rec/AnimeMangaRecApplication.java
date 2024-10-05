c629acbfa16b76d51d4fa9b340fb9ea68c76b45d
denvicar <30330046+denvicar@users.noreply.github.com>
2024-10-05 21:08:13 +0200

    First commit, bootstrap project

---
+++ src/main/java/com/denvicar/anime_manga_rec/AnimeMangaRecApplication.java
@@ -0,0 +1,13 @@
+package com.denvicar.anime_manga_rec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeMangaRecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeMangaRecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denvicar/anime_manga_rec/AnimeMangaRecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denvicar.anime_manga_rec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeMangaRecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
